5ae0d4a1a52ad0d45422c2e6fb0c473f38fceb6c
Pauline Lopez <paulinelopez31650@gmail.com>
2022-02-05 09:22:58 +0100

    compute date event

--- SiteJu/Migrations/Reservation/20220204141246_Duration.Designer.cs
+++ SiteJu/Migrations/Reservation/20220204141246_Duration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiteJu.Data;
 
@@ -10,9 +11,10 @@ using SiteJu.Data;
 namespace SiteJu.Migrations.Reservation
 {
     [DbContext(typeof(ReservationContext))]
-    partial class ReservationContextModelSnapshot : ModelSnapshot
+    [Migration("20220204141246_Duration")]
+    partial class Duration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
