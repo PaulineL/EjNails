953c0f2a2bf2c9c499f412a846da9803c9c42d26
Pauline <72314903+PaulineL@users.noreply.github.com>
2022-02-05 13:51:17 +0100

    remove comments

--- SiteJu/Program.cs
+++ SiteJu/Program.cs
@@ -15,9 +15,6 @@ namespace SiteJu
     {
         public static void Main(string[] args)
         {
-
-            //CreateHostBuilder(args).Build().Run();
-
             var host = CreateHostBuilder(args).Build();
 
             CreateDbIfNotExists(host);
